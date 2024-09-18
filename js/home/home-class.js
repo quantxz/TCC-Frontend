855307b7d6fe2cd7c0009f7e02ba7dff795a559d
quantxz <indiedev.core@gmail.com>
2024-09-18 09:50:38 -0300

    profile pic user in post update

--- js/home/home-class.js
+++ js/home/home-class.js
@@ -53,7 +53,7 @@ class HomeFunctions {
         postElement.innerHTML = `
             <div class="post-header">
                 <div class="post-profile-pic">
-                    <img src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg" alt="">
+                    <img src="/assets/download.png" alt="">
                 </div>
                 <div class="post-profile-infos">
                     <p>@${data.authorName ? data.authorName : "undefined"}</p>
